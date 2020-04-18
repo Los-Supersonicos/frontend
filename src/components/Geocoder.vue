0394d47bfb2ee5028421d90b514db2cee1c05f27
NicoAbitante <nicolas.abitante@devsar.com>
2020-04-18 19:29:42 -0300

    added geocoder component

---
+++ src/components/Geocoder.vue
@@ -0,0 +1,30 @@
+<template>
+    <section>
+        <form @submit="getPoints()">
+            <div class="form-group">
+                <label for="geocode">Ingrese ubicación</label>
+                <input v-model="code" type="text" class="form-control" id="geocode" placeholder="">
+            </div>
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </form>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'Geocoder',
+    data() {
+        return {
+            code: ''
+        }
+    },
+    methods: {
+        getPoints() {
+            fetch('https://maps.googleapis.com/maps/api/geocode/json?address='+this.code+'&key=AIzaSyC50wRXPH7ee92T9118G722gKyvaf8NFQw').then((res) =>{
+                console.log(res.json())
+            });
+        
+        }
+    }
+}
+</script>
