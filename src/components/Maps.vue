022b63306a9c8769e748c2e19d088d3bc34a3511
Emiliano Dalla Verde Marcozzi <6564766d@gmail.com>
2020-04-18 20:55:16 -0300

    maps is workin

--- src/components/Maps.vue
+++ src/components/Maps.vue
@@ -1,74 +1,78 @@
 <template>
-  <div class="App"/>
+  <div class="App" />
 </template>
 
 <script>
+/* eslint-disable */
 import { publications } from "@/api";
-import gmapsInit from '@/utils/gmaps';
-var infoWindow
-var map
-var pos = {
-              lat: -37.0258667,
-              lng: -81.5996656
-            };
-export default {
-  name: 'App',
-  async created() {
+import gmapsInit from "@/utils/gmaps";
 
-  },
+var map;
+var pos = {
+  lat: -37.0258667,
+  lng: -81.5996656
+};
+
+export default {
+  name: "Maps",
   data() {
-        return {
-            google: ''
-        }
-    },
+    return {
+      google: ""
+    };
+  },
   async mounted() {
     try {
       var google = await gmapsInit();
-      
 
-        // Try HTML5 geolocation.
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
+      debugger;
+
+      // Try HTML5 geolocation.
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          function(position) {
             pos = {
               lat: position.coords.latitude,
               lng: position.coords.longitude
             };
-            map.setZoom(15)
+            map.setZoom(15);
             map.setCenter(pos);
-            fetchMarkers()
-            
-          }, function() {
-            handleLocationError(true, infoWindow,map.getCenter());
-          });
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-        }
+            fetchMarkers();
+          },
+          function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          }
+        );
+      } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+      }
       map = google.maps.Map(this.$el);
     } catch (error) {
       console.error(error);
     }
-    this.google = google
-  },
+    this.google = google;
+  }
 };
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
-        infoWindow.open(map);
-      }
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
+}
 async function fetchMarkers() {
-    const response = await publications.fetchAll()
-    console.log(response)
-    response.data.features.map(x => {
-        publicationToMarker(x)
-    })
-      }
-function publicationToMarker(publication){
-    console.log(publication.geometry.coordinates)
-    pos = publication.geometry.coordinates
-    this.google.maps.Marker({position: {lat: pos[0], lng: pos[1]}, map: map});
+  const response = await publications.fetchAll();
+  console.log(response);
+  response.data.features.map(x => {
+    publicationToMarker(x);
+  });
+}
+function publicationToMarker(publication) {
+  console.log(publication.geometry.coordinates);
+  pos = publication.geometry.coordinates;
+  this.google.maps.Marker({ position: { lat: pos[0], lng: pos[1] }, map: map });
 }
 </script>
 
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,7 @@
 import Home from "./components/Home";
-import Maps from "./components/Maps"
+import Maps from "./components/Maps2";
 
-export const routes = [{ path: "/", component: Home, name: "home" }, { path: "/maps", component: Maps, name: "maps" }];
-
+export const routes = [
+  { path: "/", component: Home, name: "home" },
+  { path: "/maps", component: Maps, name: "maps" },
+];
