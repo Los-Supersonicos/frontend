2d5d06ec0a3c52b34760b140709d57de6d135cc5
Emiliano Dalla Verde Marcozzi <6564766d@gmail.com>
2020-04-18 16:09:27 -0300

    add a new publication is working

--- src/components/PublicationModal.vue
+++ src/components/PublicationModal.vue
@@ -1,45 +1,49 @@
 <template>
-  <b-modal :id="id" :title="title" ok-title="Publicar" ok-variant="info" size="lg">
-    <p>Pana aca va el geolocator que tas haciendo</p>
-
-    <b-form-group id="input-group-3" label="Categoria:" label-for="input-3">
-      <b-form-select id="input-3" v-model="form.food" :options="categories" required></b-form-select>
+  <b-modal :id="id" :title="title" ok-title="Publicar" ok-variant="info" size="lg" hide-footer>
+    <b-form-group id="input-group-1" label="En donde se necesita ayuda?" label-for="input-1">
+      <p>Pana aca va el geolocator que tas haciendo</p>
     </b-form-group>
+
+    <!-- Si se hacen categorias, las soportamoss -->
+    <!-- <b-form-group id="input-group-3" label="Categoria:" label-for="input-3">
+      <b-form-select id="input-3" v-model="publication.category" :options="categories" required></b-form-select>
+    </b-form-group>-->
 
     <b-form-group id="input-group-2" label="Descripcion" label-for="input-2">
       <b-form-textarea
         id="textarea"
-        v-model="text"
+        v-model="publication.description"
         placeholder="En que se puede ayudar?"
         rows="3"
         max-rows="6"
       ></b-form-textarea>
     </b-form-group>
+
+    <b-button variant="info" @click="addPublication">Publicar</b-button>
   </b-modal>
 </template>
 
 <script>
+import { publications } from "@/api";
+import { getPoint } from "@/geo";
+
 export default {
   name: "PublicationModal",
   methods: {
-    onReset: () => {
-      console.log("reset form");
-    },
-    onSubmit: () => {
-      console.log("submit form");
+    addPublication: async function() {
+      const resp = await publications.add(this.publication);
+      console.log(resp);
     }
   },
   props: ["id", "title"],
   data() {
     return {
-      form: {
-        name: "zxc",
-        food: "qwe"
+      publication: {
+        description: "",
+        type: "N",
+        location: getPoint("-35.5860008", "-58.0837458")
       },
-      categories: ["Alimentos", "Ropa"],
-      text: "",
-      type: "N",
-      show: true
+      categories: ["Alimentos", "Ropa"]
     };
   }
 };
