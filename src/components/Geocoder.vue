63c573ef33ca289b0d06b1ec088985585a23a6bb
Emiliano Dalla Verde Marcozzi <6564766d@gmail.com>
2020-04-19 15:27:54 -0300

    locate user on `enter` (at geocode component)

--- src/components/Geocoder.vue
+++ src/components/Geocoder.vue
@@ -1,11 +1,12 @@
 <template>
-  <b-form-group
-    id="input-group-1"
-    :label="label"
-    label-for="input-1"
-  >
+  <b-form-group id="input-group-1" :label="label" label-for="input-1">
     <b-form inline>
-      <b-form-input v-model="address" placeholder="Direccion" class="mr-2"></b-form-input>
+      <b-form-input
+        @keyup.enter="getLocation"
+        v-model="address"
+        placeholder="Direccion"
+        class="mr-2"
+      ></b-form-input>
       <b-button @click="getLocation">Buscar</b-button>
     </b-form>
   </b-form-group>
@@ -19,7 +20,7 @@ export default {
   data() {
     return {
       address: "",
-      position: null,
+      position: null
     };
   },
   methods: {
@@ -31,7 +32,7 @@ export default {
       }
       this.position = latLong ? latLong : null;
       this.$emit("position", latLong);
-    },
-  },
+    }
+  }
 };
 </script>
